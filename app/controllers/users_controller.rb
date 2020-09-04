f2d61d79ac0f4535db8184cebd3c8a9cc3d43d98
Joseph Burke <joseph_burke@outlook.com>
2020-09-04 16:42:58 +0100

    Add if/else statement to UsersController#update method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,8 +19,12 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to action: 'new'
+
+    if @user.update(user_params)
+      redirect_to edit_user_path
+    else
+      render :edit
+    end
   end
 
   private
