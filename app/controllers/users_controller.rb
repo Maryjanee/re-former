02c31a638b299a8639a650a2d085bad120075c17
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-09-04 08:52:44 +0100

    Add validations and private method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,13 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(user_params)
+    
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+    
   end
 
   private
